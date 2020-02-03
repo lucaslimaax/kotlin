43e5b11e80ff6fa92a1055d878577fabd8543692
Lucas Lima <38673203+lucaslimaax@users.noreply.github.com>
2020-02-03 16:32:56 -0300

    first file

---
+++ variaveis.kt
@@ -0,0 +1,20 @@
+/**
+ * You can edit, run, and share this code. 
+ * play.kotlinlang.org 
+ */
+
+fun main() {
+    
+    // var para variaveis mutaveis
+   var usuario = "Lucas"
+   usuario = "Aline"
+   println(usuario)
+   
+   //val para variaveis imutaveis
+   val pi = 3.14
+
+    var salario = 100
+    val bonus = 30
+    var total = salario+bonus
+    println(total)
+}
