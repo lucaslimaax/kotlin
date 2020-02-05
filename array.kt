5f8b5d8c35e4d4021d40b46126de5cc35ff1017e
Lucas Lima <38673203+lucaslimaax@users.noreply.github.com>
2020-02-05 09:30:58 -0300

    new file- array

---
+++ array.kt
@@ -0,0 +1,5 @@
+
+fun main() {
+   var nomes = arrayOf("Lucas","Matheus", "Anderson")
+   println(nomes[0])
+}
---
+++ tiposVariaveis.kt
@@ -0,0 +1,19 @@
+/**
+ * You can edit, run, and share this code. 
+ * play.kotlinlang.org 
+ */
+
+fun main() {
+    
+    // o Kotlin atribui o tipo da varivel como acontece no python
+    
+    //var do tipo float 
+var numero = 3.545f
+    
+    //var foi modificada para string usando os dois pontos
+    var texto: String = "Lucas Lima"
+    var numerolong :long = 1000L
+    
+    println(texto)
+    println(numero)
+}
