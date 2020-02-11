618280e8464d232f163309aa8b030ac6e3697b32
Lucas Lima <38673203+lucaslimaax@users.noreply.github.com>
2020-02-11 14:15:58 -0300

    exemplos de loops utilizando for e while

--- lacos.kt
+++ lacos.kt
@@ -1,1 +1,31 @@
-teste
+/*
+Loops
+ while
+    for
+*/
+fun main(args: Array<String>) {
+ /*
+    var numero = 1
+    while( numero < 5 ){
+        //códigos a serem executados
+        println("Executado: $numero")
+        numero++
+    }*/
+    
+    /*
+    for( numero in 1..5 ){
+        println("Executado: $numero")
+    }*/
+    
+    val postagens = arrayOf(
+       "Descoberto novo método de tratamento para Diabetes!", 
+       "Novo curso de Android foi lançado!",
+       "Bolsa de valores opera em alta de 2%.",
+        "Postagem adicionada"
+    )
+    
+    for( (indice, postagem) in postagens.withIndex() ){
+        println( "$indice - $postagem" )
+    }
+    
+}
